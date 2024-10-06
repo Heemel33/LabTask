1720b11a7ba8202b3d4349791787908b0be21abf
Heemel33 <hasibulislamhemel12@gmail.com>
2024-10-06 19:07:18 +0600

    Create CtoF.js

---
+++ CtoF.js
@@ -0,0 +1,7 @@
+function convert(x) {
+    let fahrenhit = (x * 9 / 5) + 32;
+    return Fahrenheit;
+}
+let Celsius = parseInt(prompt("Enter the value of Celsius: "));
+
+console.log("The value in Fahrenheit is : ", convert(Celsius));
