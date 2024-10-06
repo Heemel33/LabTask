43153c8279d9ca014364c2f90f5c5de5a5f050db
Heemel33 <hasibulislamhemel12@gmail.com>
2024-10-06 19:29:46 +0600

    Update FindingmaxValue.js

--- FindingmaxValue.js
+++ FindingmaxValue.js
@@ -1,14 +1,11 @@
-function findLargest(arr) {
-    let max_val = arr[0];
+let arr = [10, 17, 27, 1, 8];
+let maxvalue = arr[0];
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] > max_val) {
-            max_val = arr[i];
-        }
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxvalue) {
+        maxvalue = arr[i];
     }
-
-    return max_val;
 }
 
-let arr = [10, 5, 20, 8, 15];
-console.log(" largest number is: ", findLargest(arr));
+console.log("The max value is : ", maxvalue);
+
